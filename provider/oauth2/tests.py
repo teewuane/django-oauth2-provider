fea40aa235aece2a5556c60c53ca21bbb32f016a
Travis Millward <travismillward@mac.com>
2017-02-28 03:03:28 -0700

    Changed some syntax to be compatible with python 3 and reverse compatible with python 2.7 (possibly python 2.6 as well).

--- provider/oauth2/tests.py
+++ provider/oauth2/tests.py
@@ -1,5 +1,10 @@
 import json
-import urlparse
+
+try:
+    import urlparse
+except ImportError:
+    from urllib.parse import urlparse
+
 import datetime
 from django.http import QueryDict
 from django.conf import settings
