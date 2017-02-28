eaadc260485e94ef5e367f1717057f39f6f3b8fb
Travis Millward <travismillward@mac.com>
2017-02-28 03:08:38 -0700

    fixed some import issues with python 3 that will still work with python 2

--- provider/oauth2/__init__.py
+++ provider/oauth2/__init__.py
@@ -1,6 +1,6 @@
-import backends
-import forms
-import managers
-import models
-import urls
-import views
+from . import backends
+from . import forms
+from . import managers
+from . import models
+from . import urls
+from . import views
